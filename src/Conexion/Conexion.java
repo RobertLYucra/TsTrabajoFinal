98434b1577437a5b87c5e25f464127f23fd7eb75
juanp <juanp@192.168.1.30>
2021-06-22 17:31:42 -0500

    conexion

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -5,10 +5,27 @@
  */
 package Conexion;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author RobertLY
+ * @author juanp
  */
 public class Conexion {
-    
+    public static Connection ObtenerConexion(){
+        Connection cn=null;//No hay conexion
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            cn=DriverManager.getConnection("jdbc:mysql://localhost:3306/colegio","root","");//verificar la conexion
+            System.out.println("Conexion exitosa!!!!");
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Error al conectar a la BD:"+ex.getMessage());
+        }
+        return cn;//si hay una  conexion
+    }    
 }
