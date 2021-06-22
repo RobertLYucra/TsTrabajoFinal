8a9787c7ef0a1be78b6145b16ce5720ff0a0c228
PC <PC@JFloresL>
2021-06-22 17:34:45 -0500

    clase Alumno

--- src/Beans/Alumno.java
+++ src/Beans/Alumno.java
@@ -10,8 +10,51 @@ package Beans;
  * @author RobertLY
  */
 public class Alumno {
+    private String cod;
     private String name; 
-    private String apellido;
-    private String prueba;
     private String email;
+    private String password;
+
+    public Alumno() {
+    }
+
+    public Alumno(String cod, String name, String email, String password) {
+        this.cod = cod;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 }
